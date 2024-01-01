fa9b5173cedca87036bc23b8ea094a366b67e3a4
syedafa <afwaan3@gmail.com>
2024-01-01 12:28:33 +0530

    added some css styles

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -7,13 +7,16 @@ import PreviousCard from "./PreviousCard";
 import UnknowCard from "./UnknowCard";
 const Battle = () => {
   const { pokemon } = useContext(context);
+  const [numberOfCards, setNumberOfCards] = useState();
+  const [number, setNumber] = useState();
+  const [error, setError] = useState(false);
 
   function getRandom(arr, n) {
     var result = new Array(n),
       len = arr.length,
       taken = new Array(len);
     if (n > len)
-      throw new RangeError("getRandom: more elements taken than available");
+      throw new RangeError("getRandom : more elements taken than available");
     while (n--) {
       var x = Math.floor(Math.random() * len);
       result[n] = arr[x in taken ? taken[x] : x];
@@ -57,75 +60,103 @@ const Battle = () => {
   const [previousCard, setPreviousCard] = useState(null);
 
   const resetGame = () => {
+    setNumberOfCards(null);
     setResetBoard(!resetBoard);
   };
+  const totalCardHandler = (e) => {
+    e.preventDefault();
+    if (isNaN(number)) {
+      return setError(true);
+    }
 
+    setNumberOfCards(number);
+  };
   useEffect(() => {
-    const array1 = pokemon.length > 9 ? getRandom(pokemon, 10) : [];
+    const array1 = pokemon.length > 9 ? getRandom(pokemon, number) : [];
     setPlayer1(array1);
-    const array2 = pokemon.length > 9 ? getRandom(pokemon, 10) : [];
+    const array2 = pokemon.length > 9 ? getRandom(pokemon, number) : [];
     setPlayer2(array2);
-  }, [resetBoard]);
+  }, [resetBoard, numberOfCards]);
 
   // console.log(previousCard);
   console.log("player2", player2);
   console.log("player1", player1);
-
+  console.log(numberOfCards);
   return (
-    <div>
-      <div className="top">
-        <div className="top-left">
-          <div>card remaining:{player1?.length}</div>
-          <h2>player 1</h2>
-        </div>
+    <>
+      {numberOfCards ? (
+        <div>
+          <div className="top">
+            <div className="top-left">
+              <div>card remaining:{player1?.length}</div>
+              <h2>player 1</h2>
+            </div>
 
-        <div className="middle">
-          <div className="title">previous cards</div>
-          <div className="previous-cards">
-            {previousCard &&
-              previousCard.map((ele) => (
-                <PreviousCard key={ele.id} data={ele.data} />
-              ))}
+            <div className="middle">
+              <div className="title">previous cards</div>
+              <div className="previous-cards">
+                {previousCard &&
+                  previousCard.map((ele) => (
+                    <PreviousCard key={ele.id} data={ele.data} />
+                  ))}
+              </div>
+            </div>
+            <div className="top-right">
+              <div>{player2?.length}:card remaining</div>
+              <h2>player 2</h2>
+            </div>
           </div>
-        </div>
-        <div className="top-right">
-          <div>{player2?.length}:card remaining</div>
-          <h2>player 2</h2>
-        </div>
-      </div>
-      {player1?.length > 0 && player2?.length > 0 ? (
-        <div className="btw">
-          <div className="left">
-            {player1Turn ? (
-              <BattleCard player1={player1} checkResult={checkResult} />
-            ) : (
-              <UnknowCard />
-            )}
-          </div>
+          {player1?.length > 0 && player2?.length > 0 ? (
+            <div className="btw">
+              <div className="left">
+                {player1Turn ? (
+                  <BattleCard player1={player1} checkResult={checkResult} />
+                ) : (
+                  <UnknowCard />
+                )}
+              </div>
 
-          <div className="right">
-            {!player1Turn ? (
-              <BattleCard player2={player2} checkResult={checkResult} />
-            ) : (
-              <UnknowCard />
-            )}
-          </div>
+              <div className="right">
+                {!player1Turn ? (
+                  <BattleCard player2={player2} checkResult={checkResult} />
+                ) : (
+                  <UnknowCard />
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="winner-parent">
+              <div className="winner">
+                {`${player1Turn ? "player 1" : "player 2"} is the winner`}
+              </div>
+              <button onClick={resetGame} className="rst-game">
+                play again
+              </button>
+            </div>
+          )}
+
+          <Link to="/">
+            <button className="go-back">go back</button>
+          </Link>
         </div>
       ) : (
-        <div className="winner-parent">
-          <div className="winner">
-            {`${player1Turn ? "player 1" : "player 2"} is the winner`}
-          </div>
-          <button onClick={resetGame} className="rst-game">
-            play again
-          </button>
+        <div className="form">
+          <form onSubmit={totalCardHandler}>
+            <h2>Please enter the number of card</h2>
+            <input
+              type="text"
+              placeholder="Enter the number of card "
+              className="total-card"
+              onChange={(e) => {
+                setNumber(e.target.value);
+                setError(false);
+              }}
+            />
+          </form>
+          {error && <p>Please enter a valid number</p>}
         </div>
       )}
-
-      <Link to="/">
-        <button className="go-back">go back</button>
-      </Link>
-    </div>
+    </>
   );
 };
 
--- src/components/BattleCard.jsx
+++ src/components/BattleCard.jsx
@@ -21,68 +21,70 @@ const BattleCard = ({ player1, player2, checkResult }) => {
   // const checkResultSpeed = () => {
   //   return player1[0].data.hp > player2[0].data.hp;
   // };
-
+  console.log(player1, ": player 1");
+  console.log(player2, ": player 2");
   return (
     <>
       <div className="crd">
         <h1>
           {player1
-            ? player1[0].data.name
+            ? player1[0]?.data.name
             : player2
-            ? player2[0].data.name
+            ? player2[0]?.data.name
             : "dgeded"}
         </h1>
         <img
           src={
             player1
-              ? player1[0].data.img
+              ? player1[0]?.data.img
               : player2
-              ? player2[0].data.img
+              ? player2[0]?.data.img
               : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
           }
           alt=""
         />
         <h3 onClick={() => checkResult("hp")}>
-          hp:{player1 ? player1[0].data.hp : player2 ? player2[0].data.hp : 56}
+          hp:
+          {player1 ? player1[0]?.data.hp : player2 ? player2[0]?.data.hp : 56}
         </h3>
         <h3 onClick={() => checkResult("attack")}>
           attack:
           {player1
-            ? player1[0].data.attack
+            ? player1[0]?.data.attack
             : player2
-            ? player2[0].data.attack
+            ? player2[0]?.data.attack
             : 67}
         </h3>
         <h3 onClick={() => checkResult("defence")}>
           defence:
           {player1
-            ? player1[0].data.defence
+            ? player1[0]?.data.defence
             : player2
-            ? player2[0].data.defence
+            ? player2[0]?.data.defence
             : 78}
         </h3>
         <h3 onClick={() => checkResult("special_attack")}>
           special-attack:
           {player1
-            ? player1[0].data.special_attack
+            ? player1[0]?.data.special_attack
             : player2
-            ? player2[0].data.special_attack
+            ? player2[0]?.data.special_attack
             : 56}
         </h3>
         <h3 onClick={() => checkResult("special_defense")}>
           special-defence:
           {player1
-            ? player1[0].data.special_defense
+            ? player1[0]?.data.special_defense
             : player2
-            ? player2[0].data.special_defense
+            ? player2[0]?.data.special_defense
             : 89}
         </h3>
         <h3 onClick={() => checkResult("speed")}>
           speed:
           {player1
-            ? player1[0].data.speed
+            ? player1[0]?.data.speed
             : player2
-            ? player2[0].data.speed
+            ? player2[0]?.data.speed
             : 90}
         </h3>
       </div>
